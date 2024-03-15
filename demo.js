7f81e33ed003b45ebb272eaec92fe29449aebff7
muhammedshibilit <shibilibava89@gmail.com>
2024-03-15 10:55:49 +0530

    task

---
+++ demo.js
@@ -0,0 +1,125 @@
+
+//circle
+
+// let r=5;
+// let pi=3.14
+// console.log("radius",r);
+// let d=2*r
+// console.log(d);
+// let c=2*pi*r
+// console.log(c);
+// let a=pi*r*r
+// console.log(a);
+
+
+
+// //simple interest
+
+// let p=10000
+// let r=10
+// let t=12
+
+// let i=(p*r*t)/100
+// console.log("simple interest" ,i);
+
+
+
+
+// //leap year
+
+
+// let a=2200
+
+// if((a%400==0) || (a%4==0 && a%100!=0)){
+//     console.log(a ," is a leap year" );
+
+// }
+// else{
+//     console.log(a ,"is not a leap year" );
+
+// }
+
+
+//number exchange
+
+// let a=10
+// let b=20
+
+// let c=a
+// a=b
+
+// b=c
+// console.log(a,b);        
+
+// //fahrenheit
+
+// function toFahrenheit(c){
+//     return (9/5*c)+32
+// }
+
+// let value = toFahrenheit(100)
+
+// console.log(value);
+
+
+// //prime number
+
+// let n,i,flag;
+// flag=1;
+// i=2;
+// n=1;
+// while(i<n){
+//     if(n%i==0){
+//     flag=0;
+//     console.log(n+" is not a prime number");
+//     break;
+//     }
+//     else{
+//         i++;
+//     }
+        
+    
+// }
+//     if(flag==1){
+//         console.log(n+ "is a prime number");
+//     }
+
+
+
+
+
+
+
+// // Function to check if a number is prime
+// function isPrime(num) {
+//     if (num <= 1) {
+//         return false;
+//     }
+//     for (let i = 2; i <= Math.sqrt(num); i++) {
+//         if (num % i === 0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// // Function to display prime numbers between two intervals
+
+// function displayPrimeNumbers(start, end) {
+//     console.log(`Prime numbers between ${start} and ${end}:`);
+//     for (let i = start; i <= end; i++) {
+//         if (isPrime(i)) {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// // Example usage
+// const startInterval = 10;
+// const endInterval = 20;
+// displayPrimeNumbers(startInterval, endInterval);
+
+
+
+
+
