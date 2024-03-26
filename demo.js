be70baf608fe66e71d6a9d0c6e75bd79bbaf780e
muhammedshibilit <shibilibava89@gmail.com>
2024-03-26 12:24:32 +0530

    task

--- demo.js
+++ demo.js
@@ -40,7 +40,7 @@
 // }
 
 
-//number exchange
+// // number exchange
 
 // let a=10
 // let b=20
@@ -121,5 +121,51 @@
 
 
 
+// // //products
+
+let products=[
+    {pid:100,pName:'apple',band:'5g',price:120000,display:'led'},
+    {pid:101,pName:'samsaung',band:'5g',price:45000,display:'led'},
+    {pid:102,pName:'blackberry',band:'4g',price:50000,display:'led'},
+    {pid:103,pName:'nokia',band:'3g',price:1200,display:'lcd'},
+    {pid:104,pName:'motorola',band:'4g',price:10000,display:'lcd'}
+]
+
+// // //product name only
+// // let product1=products.map(product1=>product1.pName)
+// // console.log(product1)
+
+// // //details of product with lcd display
+// let product2=products.filter(product2=>product2.display==="lcd")
+// console.log(product2)
+
+// //print 5g mobile
+
+// let product3=products.filter(p=>p.band==="5g")
+// product3.forEach((p)=>console.log(p.price,p.pName))
 
 
+// //filter mobiles based on prices 
+
+// let minPrice=1000
+// let maxPrice=10000
+// let product4=products.filter(p=>p.price>=minPrice&&p.price<=maxPrice)
+// product4.forEach((p)=>console.log(p.pName))
+
+
+// costly mobile
+
+let costlyproduct = products.reduce((minPriceproduct,currentProduct)=>{
+    return(currentProduct.price > minPriceproduct.price)? currentProduct:minPriceproduct;
+},products[0]);
+
+console.log("costly mobile:",costlyproduct.pName);
+
+
+// low cost mobile
+
+let lowcostproduct = products.reduce((maxPriceproduct,currentProduct)=>{
+    return(currentProduct.price < maxPriceproduct.price)? currentProduct:maxPriceproduct;
+},products[0]);
+
+console.log("costly mobile:",lowcostproduct.pName);
